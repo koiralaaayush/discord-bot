4d1fa223f4ee5e87ea227e37f40236bf53a9e5ad
koiralaaayush <koiralaaayush1@gmail.com>
2020-12-20 16:54:11 -0800

    t

--- bot.py
+++ bot.py
@@ -9,24 +9,24 @@ a_file = open("script.txt")
 lines = a_file.readlines()
 today = date.today()
  
-client = commands.Bot(command_prefix = ">")
+bot = commands.Bot(command_prefix = ">")
 
-@client.event
+@bot.command()
+async def foo(ctx, arg):
+    await ctx.send(arg)
+
+@bot.event
 async def on_ready():
-  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Cat Videos"))
-  print('We have logged in as {0.user}'.format(client))
+  await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Cat Videos"))
+  print('We have logged in as {0.user}'.format(bot))
 
-@client.command()
-async def test(ctx):
-  await ctx.scend("123")
-
-@client.event
+@bot.event
 async def on_message(message):
 
   if message.content.startswith('shrug'):
     await message.channel.send('¯\_(ツ)_/¯')
 
-  if message.content.startswith('client'):
+  if message.content.startswith('bot'):
     await message.channel.send('How can I help you today?')
 
   if message.content.startswith('today'):
@@ -43,7 +43,7 @@ async def on_message(message):
   if message.content.startswith('hello'):
     await message.channel.send('https://cataas.com/cat/says/hello%20world!')
 
-    await client.process_commands(message)
+    await bot.process_commands(message)
 
-client.run(os.getenv('TOKEN'))
+bot.run(os.getenv('TOKEN'))
 
